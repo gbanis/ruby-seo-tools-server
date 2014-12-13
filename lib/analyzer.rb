2b5ddda131d624324abe90eda98aa7503f2d9747
George <gbanis@gmail.com>
2014-12-13 16:50:22 -0500

    Implemented #title_contains_keyword!

--- lib/analyzer.rb
+++ lib/analyzer.rb
@@ -1,12 +1,16 @@
 module Analyzer
   def analyze
     title_good_length!
-
+    title_contains_keyword!
   end
 
   def title_good_length!
     length = self.title.length
     self.title_good_length = length > 40 && length < 70
+  end
+
+  def title_contains_keyword!
+    self.title_contains_keyword = self.title.downcase.include? self.keyword
   end
 
 end
