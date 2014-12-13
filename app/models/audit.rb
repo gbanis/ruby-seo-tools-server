9c0584a65477f8ca0fa0c792aa04cafe7d2f6385
George <gbanis@gmail.com>
2014-12-13 14:20:15 -0500

    Implemented Nokogiri. Successfully saves audit.title in DB

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -1,12 +1,18 @@
 class Audit < ActiveRecord::Base
 
   def generate
-    get_html
+    @parsed_html = Nokogiri::HTML(set_raw_html)
+    set_metrics
     self.save
   end
 
   private
-  def get_html
-    self.page_content = HTTParty.get(self.url)
+
+  def set_metrics
+    self.title = @parsed_html.title
+  end
+
+  def set_raw_html
+    self.raw_html = HTTParty.get(self.url)
   end
 end
