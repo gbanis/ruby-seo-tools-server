3c6a5b197b190dafa8d81f533d6a80255b79cad6
George <gbanis@gmail.com>
2014-12-13 13:01:02 -0500

    Created basic Audits Controller

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,4 +1,13 @@
 class AuditsController < ApplicationController
+  def create
+    @audit = Audit.create(audit_params)
+    render json: @audit
+  end
 
+  private
+
+  def audit_params
+    params.permit(:url, :keyword)
+  end
 end
 
