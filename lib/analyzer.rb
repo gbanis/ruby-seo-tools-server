59465821f50a53b47f72d0c490cdd89dee66e2a7
George <gbanis@gmail.com>
2014-12-14 13:47:48 -0500

    Minor tweaks in the analyzer metric tolerance.

--- lib/analyzer.rb
+++ lib/analyzer.rb
@@ -3,6 +3,7 @@ module Analyzer
 
   def analyze
     path_contains_keyword!
+    path_no_underscores!
     title_good_length!
     title_contains_keyword!
     meta_description_good_length!
@@ -14,9 +15,14 @@ module Analyzer
     self.path_contains_keyword = path.gsub(/\W/, " ").downcase.include? self.keyword.downcase
   end
 
+  def path_no_underscores!
+    path = URI(self.url).path
+    self.path_no_underscores = !(path.include? "_")
+  end
+
   def title_good_length!
     length = self.title.length
-    self.title_good_length = length > 40 && length < 70
+    self.title_good_length = length > 40 && length < 60
   end
 
   def title_contains_keyword!
@@ -25,7 +31,7 @@ module Analyzer
 
   def meta_description_good_length!
     length = self.meta_description.length
-    self.meta_description_good_length = length > 150 && length < 160
+    self.meta_description_good_length = length > 130 && length < 156
   end
 
   def meta_description_contains_keyword!
