3755b11be4ac254f49c71e30f5c8bce9db4cd699
George <gbanis@gmail.com>
2014-12-14 14:47:21 -0500

    Passing tests for Audit#analyze

--- spec/models/audit_spec.rb
+++ spec/models/audit_spec.rb
@@ -16,15 +16,12 @@ RSpec.describe Audit, type: :model do
         it "receives and sets raw_html" do
           expect(@audit.raw_html).not_to be nil
         end
-
         it "sets the correct title" do
           expect(@audit.title).to eq "How To Make Espresso At Home The Inexpensive Way"
         end
-
         it "sets the correct meta_description" do
           expect(@audit.meta_description).to eq "Everybody wants to know how to make espresso at home. Especially now that it has become quite expensive to buy coffee on the road ..."
         end
-
         it "sets the correct meta_description" do
           expect(@audit.meta_description).to eq "Everybody wants to know how to make espresso at home. Especially now that it has become quite expensive to buy coffee on the road ..."
         end
@@ -34,7 +31,23 @@ RSpec.describe Audit, type: :model do
         it "sets the correct path_contains_keyword" do
           expect(@audit.path_contains_keyword).to be_truthy
         end
+        it "sets the correct path_no_underscores" do
+          expect(@audit.path_no_underscores).to be_truthy
+        end
+        it "sets the correct title_good_length" do
+          expect(@audit.title_good_length).to be_truthy
+        end
+        it "sets the correct title_contains_keyword" do
+          expect(@audit.title_contains_keyword).to be_truthy
+        end
+        it "sets the correct meta_description_good_length" do
+          expect(@audit.meta_description_good_length).to be_truthy
+        end
+        it "sets the correct meta_description_contains_keyword" do
+          expect(@audit.meta_description_contains_keyword).to be_truthy
+        end
       end
+
     end
   end
 end
