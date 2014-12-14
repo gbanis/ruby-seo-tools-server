139571a3c9cb9c2e50a728799c8998c63ac3ee6f
George <gbanis@gmail.com>
2014-12-14 13:46:54 -0500

    Updated audit DB fields

--- db/migrate/20141213155400_create_audits.rb
+++ db/migrate/20141213155400_create_audits.rb
@@ -2,16 +2,16 @@ class CreateAudits < ActiveRecord::Migration
   def change
     create_table :audits do |t|
       t.string :url
-      t.text :raw_html
       t.string :keyword
       t.string :title
       t.string :meta_description
-      t.boolean :path_good_length
       t.boolean :path_contains_keyword
+      t.boolean :path_no_underscores
       t.boolean :title_good_length
       t.boolean :title_contains_keyword
       t.boolean :meta_description_good_length
       t.boolean :meta_description_contains_keyword
+      t.text :raw_html
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,16 +18,16 @@ ActiveRecord::Schema.define(version: 20141213155400) do
 
   create_table "audits", force: true do |t|
     t.string  "url"
-    t.text    "raw_html"
     t.string  "keyword"
     t.string  "title"
     t.string  "meta_description"
-    t.boolean "path_good_length"
     t.boolean "path_contains_keyword"
+    t.boolean "path_no_underscores"
     t.boolean "title_good_length"
     t.boolean "title_contains_keyword"
     t.boolean "meta_description_good_length"
     t.boolean "meta_description_contains_keyword"
+    t.text    "raw_html"
   end
 
 end
