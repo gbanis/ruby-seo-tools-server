9efa18488eea7f45a5ac5c60fdf3b38dd33f80ea
George <gbanis@gmail.com>
2014-12-14 14:23:01 -0500

    Created first factory for Audit

---
+++ spec/factories/audits.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :espresso_audit, class: Audit do
+    url "http://georgebanis.com/blog/how-to-make-espresso-at-home"
+    keyword  "make espresso at home"
+  end
+end
