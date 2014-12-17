19e558f261b159a0c26625b31aedd3fd491c6e3a
George <gbanis@gmail.com>
2014-12-17 14:09:00 -0500

    Added support for posts without meta description

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -11,12 +11,21 @@ class Audit < ActiveRecord::Base
   private
 
   def set_data
-    self.title = @parsed_html.title
-    self.meta_description = @parsed_html.xpath("//meta[@name='description']/@content").first.value
+    set_title
+    set_meta_description
     self.body_text = @parsed_html.css('body').text
   end
 
   def set_raw_html
     self.raw_html = HTTParty.get(self.url)
+  end
+
+  def set_title
+    self.title = @parsed_html.title || ""
+  end
+
+  def set_meta_description
+    meta_descr = @parsed_html.xpath("//meta[@name='description']/@content").first
+    self.meta_description = meta_descr ? meta_descr.value : ""
   end
 end
